442bfed4e301d9e2004ae57d173fd3dc01823bb0
marwyr-cell <marie.wyrsch@uzh.ch>
2026-04-24 08:13:54 +0200

    Initial projecto structure

---
+++ scripts/bash/setup_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#create project strucuture
+
+mkdir -p \
+urban-sparrow-project/data/raw \
+urban-sparrow-project/data/processed \
+urban-sparrow-project/output \
+urban-sparrow-project/scripts/bash \
+urban-sparrow-project/scripts/qmd
+
